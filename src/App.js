387aeb995f32cfff5fde3356cc696ac697ffa463
Jayesh V <jayeshvartak@gmail.com>
2021-03-09 22:03:31 +0530

    somewhat working, need manual refresh

--- src/App.js
+++ src/App.js
@@ -3,26 +3,17 @@ import './App.css';
 import { API, Storage, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listCheques } from './graphql/queries';
-import { createCheque as createChequeMutation, deleteCheque as deleteChequeMutation } from './graphql/mutations';
+import { createCheque as createChequeMutation, updateCheque as updateChequeMutation, deleteCheque as deleteChequeMutation } from './graphql/mutations';
 import { onCreateCheque, onUpdateCheque } from './graphql/subscriptions';
 
-const initialFormState = { payee: '', amountWord: '' }
+const initialFormState = { payee: '', image: '', status: '0' }
 
 function App() {
-  const [Cheques, setCheques] = useState([]);
+  const [cheques, setCheques] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
     fetchCheques();
-    const subscriptionOnCreate = API.graphql(graphqlOperation(onCreateCheque)).subscribe({
-      next: apiData => {
-        console.log(apiData);
-        fetchCheques();
-//        setCheques([ ...cheques, apiData.value.data.onCreateCheque ]);
-      }
-    });
-
-    return () => subscriptionOnCreate.unsubscribe(); 
 
 /*    
     const subscriptionOnUpdate = API.graphql(graphqlOperation(onUpdateCheque)).subscribe({
@@ -59,53 +50,154 @@ function App() {
   }
 
   async function createCheque() {
-    if (!formData.payee || !formData.amountWord) return;
+//    if (!formData.payee || !formData.amountWord) return;
     await API.graphql({ query: createChequeMutation, variables: { input: formData } });
     if (formData.image) {
       const image = await Storage.get(formData.image);
       formData.image = image;
     }
-    setCheques([ ...Cheques, formData ]);
+//    setCheques([ ...cheques, formData ]);
     setFormData(initialFormState);
   }
 
+  async function uploadCheque() {
+        if (!formData.image) return;
+        formData.status = 0;
+        await API.graphql({ query: createChequeMutation, variables: { input: formData } });
+        if (formData.image) {
+          const image = await Storage.get(formData.image);
+          formData.image = image;
+        }
+        setCheques([ ...cheques, formData ]);
+        setFormData(initialFormState);
+      }
+    
+  async function recreateCheque({ id }) {
+    const newChequesArray = cheques.filter(cheque => cheque.id !== id);
+    const checkToBeDeleted = cheques.filter(cheque => cheque.id === id);
+//    setCheques(newChequesArray);
+//    formData.status = 2;
+//    createCheque();
+    checkToBeDeleted[0].status = 2;
+    checkToBeDeleted[0].id = '';
+    checkToBeDeleted[0].image = '';
+    delete checkToBeDeleted[0].id;
+    delete checkToBeDeleted[0].createdAt;
+    delete checkToBeDeleted[0].updatedAt;
+    console.log(checkToBeDeleted[0]);
+    await API.graphql({ query: createChequeMutation, variables: { input: checkToBeDeleted[0] } });
+    await API.graphql({ query: deleteChequeMutation, variables: { input: { id } }});
+  }
+
+  async function confirmCheque({ id }) {
+    console.log(id);
+    const newChequesArray = cheques.filter(cheque => cheque.id !== id);
+    const checkToBeDeleted = cheques.filter(cheque => cheque.id === id);
+//    setCheques(newChequesArray);
+//    formData.status = 2;
+//    createCheque();
+    checkToBeDeleted[0].status = 2;
+    delete checkToBeDeleted[0].createdAt;
+    delete checkToBeDeleted[0].updatedAt;
+    console.log(checkToBeDeleted[0]);
+    await API.graphql({ query: updateChequeMutation, variables: { input: checkToBeDeleted[0] } });
+    setCheques(cheques);
+}
+
   async function deleteCheque({ id }) {
-    const newChequesArray = Cheques.filter(cheque => cheque.id !== id);
+
+    const newChequesArray = cheques.filter(cheque => cheque.id !== id);
     setCheques(newChequesArray);
     await API.graphql({ query: deleteChequeMutation, variables: { input: { id } }});
   }
 
   return (
     <div className="App">
-      <h1>My Cheques App</h1>
+      <h1>Positive Pay System App</h1>
       <input
-        onChange={e => setFormData({ ...formData, 'payee': e.target.value})}
-        placeholder="Payee"
-        value={formData.payee}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'amountWord': e.target.value})}
-        placeholder="Amount in Word"
-        value={formData.amountWord}
-      />
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createCheque}>Create Cheque</button>
+              type="file"
+              onChange={onChange}
+            />
+            <br></br>
+      <button onClick={uploadCheque}>Upload scan/photo of Cheque</button>
+      <hr></hr>
       <div style={{marginBottom: 30}}>
         {
-          Cheques.map(cheque => (
+          cheques.filter(cheque => cheque.status === "1").map(cheque => (
             <div key={cheque.id || cheque.payee}>
-              <h2>{cheque.payee}</h2>
-              <p>{cheque.amountWord}</p>
-              <button onClick={() => deleteCheque(cheque)}>Delete Cheque</button>
               {
-                cheque.image && <img src={cheque.image} style={{width: 400}} />
+                cheque.image && <img src={cheque.image} alt="" style={{width: 600}} />
               }
+            <br></br>
+            <input
+              onChange={e => setFormData({ ...formData, 'payee': e.target.value})}
+              placeholder="Payee"
+              value={cheque.payee}
+            />
+            <br></br>
+            <input
+              onChange={e => setFormData({ ...formData, 'amountWord': e.target.value})}
+              placeholder="Amount in Word"
+              value={cheque.amountWord}
+            />
+            <br></br>
+            <input
+              onChange={e => setFormData({ ...formData, 'amountNumber': e.target.value})}
+              placeholder="Amount in Number"
+              value={cheque.amountNumber}
+            />
+            <br></br>
+            <input
+              onChange={e => setFormData({ ...formData, 'chequeDate': e.target.value})}
+              placeholder="Cheque Date"
+              value={cheque.chequeDate}
+            />
+            <br></br>
+            <input
+              onChange={e => setFormData({ ...formData, 'chequeNumber': e.target.value})}
+              placeholder="Cheque Number"
+              value={cheque.chequeNumber}
+            />
+            <br></br>
+              <button onClick={() => confirmCheque(cheque)}>Add PPS cheque</button>
+              <br></br>
             </div>
           ))
         }
+      </div>
+      <hr></hr>
+      <div>
+      <table border="1" align="center">
+        <thead>
+          <tr>
+            <th>Payee</th>
+            <th>Amount in Word</th>
+            <th>Amount in Number</th>
+            <th>Cheque Date</th>
+            <th>Cheque Number</th>
+            <th>Status</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          cheques.filter(cheque => cheque.status !== "").map(cheque => (
+            
+            <tr><td>{cheque.payee}</td>
+              <td>{cheque.amountWord}</td>
+              <td>{cheque.amountNumber}</td>
+              <td>{cheque.chequeDate}</td>
+              <td>{cheque.chequeNumber}</td>
+              <td>{cheque.status}</td>
+              <td>
+                <button onClick={() => deleteCheque(cheque)}>Delete cheque</button>
+              </td>
+            </tr>
+
+          ))
+        }
+        </tbody>
+      </table> 
       </div>
       <AmplifySignOut />
     </div>
